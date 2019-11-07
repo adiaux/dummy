596d76a3de42216d125a3b2193ffec2464b0afe8
adiaux <aditya8anil@gmail.com>
2019-11-07 17:58:21 +0530

    Try to fix error

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.template import RequestContext
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {})
+    pposts = {
+        'Title':posts
+    }
+    return render(request, 'blog/post_list.html', context=pposts)
+    
 
